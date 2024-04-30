7932b9e2050fba3040d099ac5a9149106e5e59a7
Samuel Ayers <sdayers@mail.fhsu.edu>
2024-04-30 16:18:48 -0500

    added authorization and login page

--- taskFlows/taskFlows/settings.py
+++ taskFlows/taskFlows/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'taskFlows.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates/registration'],
+        'DIRS': [BASE_DIR / 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
