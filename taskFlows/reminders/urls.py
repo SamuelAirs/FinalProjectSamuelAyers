8180be900f3305473e0ff89e17c5c1fc15042d50
Samuel Ayers <sdayers@mail.fhsu.edu>
2024-04-30 10:49:59 -0500

    finished part 3 of django tutorial

--- taskFlows/reminders/urls.py
+++ taskFlows/reminders/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'reminders'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:reminders_id>/", views.reminder, name="reminder"),
+    path("<int:reminders_id>/detail/", views.detail, name='detail')
 ]
--- taskFlows/reminders/views.py
+++ taskFlows/reminders/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render
+
+from .models import reminderBase
+from .models import reminderDetails
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the reminders index.")
+    latestReminderList = reminderBase.objects.order_by("reminderCreationTime")[:5]
+    template = loader.get_template("reminders/index.html")
+    context = {"latestReminderList": latestReminderList,}
+    return HttpResponse(template.render(context,request))
+
+def reminder(request, reminders_id):
+    return HttpResponse("You're looking at reminder %s." % reminders_id)
+def detail(request, reminders_id):
+    try:
+        reminder = reminderBase.objects.get(pk=reminders_id)
+        reminderD = reminderDetails.objects.get(pk=reminders_id)
+    except reminderBase.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "reminders/detail.html", {"reminder": reminder, "reminderD": reminderD})
+
+
+
