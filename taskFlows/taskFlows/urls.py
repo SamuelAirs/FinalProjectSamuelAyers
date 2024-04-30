5e80defb9f28ffa04bebaf046b37e896e1fa0d69
Samuel Ayers <sdayers@mail.fhsu.edu>
2024-04-30 16:41:24 -0500

    login page, logout redirect, and login and logout button all working

--- taskFlows/taskFlows/urls.py
+++ taskFlows/taskFlows/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("reminders/", include("reminders.urls")),
