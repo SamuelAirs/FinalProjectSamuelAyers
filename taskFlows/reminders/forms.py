3e64e02a8f1338ead0c8504c331eb2082258a22b
Samuel Ayers <sdayers@mail.fhsu.edu>
2024-05-07 16:27:02 -0500

    updated how the create reminder and edit reminder forms look

--- taskFlows/reminders/forms.py
+++ taskFlows/reminders/forms.py
@@ -5,7 +5,14 @@ class reminderForm(forms.ModelForm):
     class Meta:
         model = reminderBase
         fields = ['reminderDescription', 'reminderDetails', 'reminderDueDateStart', 'reminderDueDateEnd', 'reminderImportance']
+        labels = {
+            'reminderDescription': 'Description',
+            'reminderDetails': 'Details',
+            'reminderDueDateStart': 'Start Date',
+            'reminderDueDateEnd': 'Due Date',
+            'reminderImportance': 'Importance',
+        }
         widgets = {
-            'reminderDueDateStart': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'reminderDueDateEnd': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'reminderDueDateStart': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'reminderDueDateEnd': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
         }
